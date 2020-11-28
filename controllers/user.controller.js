ae7a7d123e724315861816e6fc17ba6292ff816c
Dahveyea <dahveyea.cowan@gmail.com>
2020-11-27 19:16:03 -0800

    Adding functionality to updating user profiles

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -315,3 +315,31 @@ module.exports = {
     }
   }
 };
+
+async updateProfile ({ request, auth, response }) {
+    try {
+        // get currently authenticated user
+        const user = auth.current.user
+
+        // update with new data entered
+        user.name = request.input('name')
+        user.username = request.input('username')
+        user.email = request.input('email')
+        user.location = request.input('location')
+        user.bio = request.input('bio')
+        user.website_url = request.input('website_url')
+
+        await user.save()
+
+        return response.json({
+            status: 'success',
+            message: 'Profile updated!',
+            data: user
+        })
+    } catch (error) {
+        return response.status(400).json({
+            status: 'error',
+            message: 'There was a problem updating profile, please try again later.'
+        })
+    }
+}
