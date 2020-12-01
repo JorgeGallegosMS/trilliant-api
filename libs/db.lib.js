06798585eb11680c08b3918681166540cb117bc6
Beck-Haywood <beck.haywood@students.makeschool.com>
2020-12-01 11:33:03 -0900

    post merge

--- libs/db.lib.js
+++ libs/db.lib.js
@@ -9,8 +9,4 @@ module.exports = () => {
   }
 
   return mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useCreateIndex: true, useFindAndModify: false});
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 29913a960917062e943e3217c609b009609faa8d
